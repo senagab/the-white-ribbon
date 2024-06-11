0748acf3b4585e79de19d6486d99752b6715ecfe
senagab <senagab@gmail.com>
2024-06-10 20:15:38 -0400

    chore: remoção de código comentado

--- src/js/main.js
+++ src/js/main.js
@@ -52,63 +52,6 @@ function updateModalContent() {
 $('#videoModal').on('hide.bs.modal', function () {
     modalVideo.pause();
 });
-        
-// // lottie
-// // Set up the options for the Intersection Observer
-// var options = {
-//     root: null,
-//     rootMargin: '0px',
-//     threshold: 0.5
-// };
-
-// // Target the section you want to observe
-// var targetSection = document.getElementById('about');
-
-// // Function to handle the intersection changes
-// function handleIntersection(entries, observer) {
-//     entries.forEach(function(entry) {
-//         if (entry.isIntersecting) {
-//             // Start the Lottie animation when the section is in view
-//             animation_desktop.play();
-//             animation_mobile.play();
-//             animation_slide.play();
-//             observer.unobserve(entry.target);
-//         }
-//     });
-// }
-
-// // Create an Intersection Observer instance
-// var observer = new IntersectionObserver(handleIntersection, options);
-
-// // Observe the target section
-// observer.observe(targetSection);
-
-// // load animation mobile
-// const animation_mobile = bodymovin.loadAnimation({
-//     container: document.getElementById('bm'),
-//     renderer: 'svg',
-//     loop: false,
-//     autoplay: false,
-//     path: '../data/json/whiteribbon.json'
-// });
-
-// // Load animation desktop
-// var animation_desktop = bodymovin.loadAnimation({
-//     container: document.getElementById('ribbon'),
-//     renderer: 'svg',
-//     loop: false,
-//     autoplay: false, // Set autoplay to false initially
-//     path: '../data/json/whiteribbon-desktop.json'
-// });
-
-// // Load animation hero (slide down)
-// var animation_slide = bodymovin.loadAnimation({
-//     container: document.getElementById('slide'),
-//     renderer: 'svg',
-//     loop: true,
-//     autoplay: true, // Set autoplay to false initially
-//     path: '../data/json/sliding-down.json'
-// });
 
 
 // Mobile Menu
