d5985a8c9edbde770497a60c88b79efcb962ab09
senagab <senagab@gmail.com>
2024-06-14 23:39:30 -0400

    feat: watch sem script

--- gulpfile.js
+++ gulpfile.js
@@ -24,6 +24,5 @@ function images(){
 exports.default = gulp.parallel(styles, images, scripts);
 
 exports.watch = function() {
-    gulp.watch('./src/styles/*.scss', gulp.parallel(styles)),
-    gulp.watch('./src/scripts/js/*.js', gulp.parallel(scripts))
+    gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
 }
