f7c486a39babfdd7e6c852939cae63101d79a552
senagab <senagab@gmail.com>
2024-07-31 13:14:02 -0400

    feat: configuração ffmpeg | gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const imagemin = require('gulp-imagemin');
 const uglify = require('gulp-uglify');
+const ffmpeg = require('gulp-fluent-ffmpeg');
 
 function scripts() {
     return gulp.src('./src/scripts/js/*.js')
@@ -21,13 +22,27 @@ function images(){
         .pipe(gulp.dest('./dist/img'));
 }
 
+function videos() {
+    return gulp.src('./src/videos/**/*.{mp4,avi,mkv}')
+        .pipe(ffmpeg('output.mp4', function (cmd) {
+            return cmd
+                .videoCodec('libx264')
+                .audioCodec('aac')
+                .format('mp4')
+                .outputOptions('-crf 28')
+                .outputOptions('-preset veryslow');
+        }))
+        .pipe(gulp.dest('./dist/videos'));
+}
+
 function watch() {
     gulp.watch('./src/styles/*.scss', styles);
     gulp.watch('./src/scripts/*.js', scripts);
     gulp.watch('./src/img/**/*', images);
+    gulp.watch('./src/videos/**/*.{mp4,avi,mkv}', videos);
 }
 
-exports.default = gulp.parallel(styles, images, scripts);
+exports.default = gulp.parallel(styles, images, scripts, videos);
 
 exports.watch = watch;
 // exports.watch = function() {
