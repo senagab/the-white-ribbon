83f0b71d2a9ff429c5edaf09ce299e5707f9e8a2
senagab <senagab@gmail.com>
2024-08-06 22:42:05 -0400

    feat: atualização seleção de videos a serem comprimidos | gulp

--- gulpfile.js
+++ gulpfile.js
@@ -32,14 +32,14 @@ function videos() {
                 .outputOptions('-crf 28')
                 .outputOptions('-preset veryslow');
         }))
-        .pipe(gulp.dest('./dist/videos'));
+        .pipe(gulp.dest('./dist/videos/'));
 }
 
 function watch() {
     gulp.watch('./src/styles/*.scss', styles);
     gulp.watch('./src/scripts/*.js', scripts);
     gulp.watch('./src/img/**/*', images);
-    gulp.watch('./src/videos/*.{mp4}', videos);
+    gulp.watch('./src/videos/**/*.{mp4}', videos);
 }
 
 exports.default = gulp.parallel(styles, images, scripts, videos);
